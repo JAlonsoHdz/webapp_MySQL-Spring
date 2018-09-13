cd42df2108822407e6239f3bd5cf81dfb62815b2
Alonso Hernandez <jesus.alonso.hernandez@gmail.com>
2018-09-13 00:59:57 -0500

    ready to package in WAR and DB connection to AWS RDS is established

--- src/test/java/com/alonso/first/application/test/AlonsoFirstTestApplicationTests.java
+++ src/test/java/com/alonso/first/application/test/AlonsoFirstTestApplicationTests.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package com.alonso.first.application.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestConfiguration
 public class AlonsoFirstTestApplicationTests {
 
 	@Test
